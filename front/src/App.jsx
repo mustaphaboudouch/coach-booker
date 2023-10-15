dc2502cfb62b8c7e16ad08f10c26f9f2e028f3a2
Mustapha Boudouch <mustaphaboudouch@gmail.com>
2023-10-15 20:22:10 +0200

    ci: configure prettier & eslint

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,9 +1,9 @@
 function App() {
-	return (
-		<div>
-			<h1 className='font-bold text-xl'>Coach Booker</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1 className="text-xl font-bold">Coach Booker</h1>
+    </div>
+  );
 }
 
 export default App;
